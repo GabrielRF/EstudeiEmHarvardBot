c08a19ced2db8a907c0a7232a853f83febc4bc68
Gabriel RF <github@gabrf.com>
2019-05-24 10:45:05 -0300

    v0

---
+++ harvard.py
@@ -0,0 +1,55 @@
+import sys
+import configparser
+import textwrap
+
+import telebot
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('bot.conf')
+
+TOKEN = config['BOT']['TOKEN']
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Digite um nome",parse_mode='HTML')
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    print(message.from_user.id)
+    Create_Image(message)
+
+def Create_Image(message):
+    bot.send_chat_action(message.from_user.id, 'upload_document')
+    try:
+        nome, curso = message.text.split(',')
+    except ValueError:
+        nome = message.text
+        curso = '@EstudeiEmHarvardBot'
+
+    nome = nome.lower().title()
+    curso = curso.title()
+
+    text = ('{}')
+    text = text.format(nome, curso)
+    text_wrap = text
+
+    fonte = ImageFont.truetype('eb-garamond.ttf', 70)
+    img = Image.open('Certificate.jpg')
+
+    draw = ImageDraw.Draw(img)
+    w, h = draw.textsize(text_wrap, font = fonte)
+    draw.text(((1650-w)/2,600), text_wrap, (0,0,0), font = fonte)
+
+    img.save(str(message.from_user.id) + '.jpg')
+    photo = open(str(message.from_user.id) + '.jpg', 'rb')
+    bot.send_photo(message.from_user.id, photo)
+
+bot.polling(none_stop=True)
+
